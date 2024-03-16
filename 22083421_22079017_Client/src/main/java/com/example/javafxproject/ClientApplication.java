8534dc43d8fcd9c3a5250656707a029798112515
JayeeshK04 <119451595+JayeeshK04@users.noreply.github.com>
2024-03-16 15:27:31 +0000

    Client program

---
+++ 22083421_22079017_Client/src/main/java/com/example/javafxproject/ClientApplication.java
@@ -0,0 +1,24 @@
+package com.example.javafxproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Class Scheduler Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
