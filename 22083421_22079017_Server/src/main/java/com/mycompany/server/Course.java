a75394f758edbccac615578f83ba1de3d869fb44
Vivianjently <vivianjently@gmail.com>
2024-03-16 15:57:28 +0000

    added course and module class
    title

---
+++ 22083421_22079017_Server/src/main/java/com/mycompany/server/Course.java
@@ -0,0 +1,11 @@
+package com.mycompany.server;
+
+public class Course {
+    String id;
+    Module[] modules = new Module[5];
+    public Course(String id){
+     this.id = id;
+
+    }
+
+}
---
+++ 22083421_22079017_Server/src/main/java/com/mycompany/server/Module.java
@@ -0,0 +1,9 @@
+package com.mycompany.server;
+
+public class Module {
+   private String id;
+    public Module(String id){
+        this.id = id;
+    }
+
+}
